b8ce908e2fceae07c46d661e714983a63cc72e4f
Puji <hollowspacesz@gmail.com>
2021-10-22 20:07:18 +0700

    add character jump movement

--- Prototype/Assets/Scripts/CharacterMovement.cs
+++ Prototype/Assets/Scripts/CharacterMovement.cs
@@ -6,14 +6,20 @@ public class CharacterMovement : MonoBehaviour
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
+    bool jump = false;
 
     private void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
