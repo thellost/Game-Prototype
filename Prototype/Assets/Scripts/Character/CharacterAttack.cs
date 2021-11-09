016cca4c67e1430d0b96e188b01a419f4dd35cd0
thellost <aldy10ball@gmail.com>
2021-11-09 22:25:02 +0700

    i fucking hate this

---
+++ Prototype/Assets/Scripts/Character/CharacterAttack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAttack : MonoBehaviour
+{
+    public Animator animator;
+    public float attackSpeed;
+    public float attackRangeCircle;
+    public Transform attackPoint;
+    public LayerMask enemiesLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //play the animation
+        animator.SetTrigger("isAttack");
+
+        //check enemies
+        Collider2D[] hit = Physics2D.OverlapCircleAll(attackPoint.position, attackRangeCircle, enemiesLayer);
+
+
+        //damage the enemies
+        foreach(Collider2D enemy in hit)
+        {
+            Debug.Log("We Hit" + enemy.name);
+        }
+    }
+}
