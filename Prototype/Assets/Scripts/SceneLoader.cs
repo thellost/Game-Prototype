4251d2af827738cb55035d27efa730778e2c8384
Puji <xsripujiastuti@gmail.com>
2021-12-01 21:08:31 +0700

    add simple map feature & test move scene

---
+++ Prototype/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "PrevRoom")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            Debug.Log("test pindah");
+        }
+        
+        else if (collision.gameObject.tag == "NextRoom")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
