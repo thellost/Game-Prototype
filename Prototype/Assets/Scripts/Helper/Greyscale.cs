295db6ecf928d2166b9acb781d24bbef604fc2d2
thellost <aldy10ball@gmail.com>
2021-12-04 16:11:20 +0700

    add greyscale shader

---
+++ Prototype/Assets/Scripts/Helper/Greyscale.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class Greyscale : MonoBehaviour
+{
+
+    private Tilemap tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        Color newColor = tilemap.color;
+        print(newColor.grayscale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
