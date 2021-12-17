3abeaad428754f7b747b0931d148e2682eab5518
Puji <xsripujiastuti@gmail.com>
2021-12-17 20:27:48 +0700

    prefab bullet + aidrone
    masih ada error belum sempet benerin hari ini:"

--- Prototype/Assets/Scripts/Enemy/Drone/BulletScript.cs
+++ Prototype/Assets/Scripts/Enemy/Drone/BulletScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Chronos;
 
 public class BulletScript : MonoBehaviour
 {
@@ -8,20 +9,33 @@ public class BulletScript : MonoBehaviour
 
     private Rigidbody2D rb;
     private GameObject target;
+    private Timeline time;
+    private Vector2 previousSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
+
         Vector2 moveDir = (target.transform.position - transform.position).normalized * bulletSpeed;
         rb.velocity = new Vector2(moveDir.x, moveDir.y);
+        previousSpeed = rb.velocity;
         Destroy(this.gameObject, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity *= time.timeScale;
+        if (time.timeScale == 1)
+        {
+            rb.velocity = previousSpeed;
+        }
+    }
+
+    public void setTimeline(Timeline timeTemp)
+    {
+        time = timeTemp;
     }
 }
---
+++ Prototype/Assets/Scripts/Enemy/Drone/TriggerAreaDrone.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAreaDrone : MonoBehaviour
+{
+    private DroneAI enemyParent;
+
+    private void Awake()
+    {
+        enemyParent = GetComponent<DroneAI>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            enemyParent.isAlerted = true;
+            gameObject.SetActive(false);
+
+        }
+    }
+}
