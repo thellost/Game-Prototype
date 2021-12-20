02984d7bda4a0a9b17f02225855017a6877a09a7
thellost <aldy10ball@gmail.com>
2021-12-21 02:34:52 +0700

    add roll aneh

--- Prototype/Assets/Scripts/Helper/CameraShake.cs
+++ Prototype/Assets/Scripts/Helper/CameraShake.cs
@@ -5,26 +5,34 @@ using Cinemachine;
 
 public class CameraShake : MonoBehaviour
 {
+    //singleton
     public static CameraShake Instance { get; private set; }
-    private CinemachineVirtualCamera cinemachineVirtualCamera;
 
+
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
     private float shaketimer;
     private float sta;
 
 
-    // Start is called before the first frame update
+   
     private void Awake()
     {
         Instance = this;
         cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
+
     // Update is called once per frame
+    //origin tempat dia muncul dimana transform.enemy
+    //text dmg nya berapa ?????
+    //jadi kamu butuh string
     public void ShakeCamera(float intensity, float time, float frequency = 1)
     {
         CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
         cinemachineBasicMultiChannelPerlin.m_FrequencyGain = frequency;
         shaketimer = time;
+
+        //instatiate
     }
 
     private void Update()
