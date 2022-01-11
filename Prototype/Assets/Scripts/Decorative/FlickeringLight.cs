12a4a3e60c841afe763f9dfcd7df1ab0a3e41624
thellost <aldy10ball@gmail.com>
2022-01-11 17:50:29 +0700

    level 1

--- Prototype/Assets/Scripts/Decorative/FlickeringLight.cs
+++ Prototype/Assets/Scripts/Decorative/FlickeringLight.cs
@@ -1,14 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering.Universal;
 using UnityEngine;
 
 public class FlickeringLight : MonoBehaviour
 {
-   // Reusable
-   
-   // Update is called once per frame
-    void Update()
+    public float lightIntensity;
+    public float flickerIntensity;
+
+    public float lightTime;
+    public float flickerTime;
+
+    System.Random rg;
+
+    Light2D flashlight;
+
+    void Awake()
     {
-        
+        rg = new System.Random();
+        flashlight = GetComponent<Light2D>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(Flicker());
+    }
+
+    IEnumerator Flicker()
+    {
+        while (true)
+        {
+            flashlight.intensity = lightIntensity;
+
+            float lightingTime = lightTime + ((float)rg.NextDouble() - 0.5f);
+            yield return new WaitForSeconds(lightingTime);
+
+            int flickerCount = rg.Next(4, 9);
+
+            for (int i = 0; i < flickerCount; i++)
+            {
+                float flickingIntensity = lightIntensity - ((float)rg.NextDouble() * flickerIntensity);
+                flashlight.intensity = flickingIntensity;
+
+                float flickingTime = (float)rg.NextDouble() * flickerTime;
+                yield return new WaitForSeconds(flickingTime);
+            }
+        }
     }
 }
