37547af4e2a41256f185d10d2d404e9958baf04a
Puji <xsripujiastuti@gmail.com>
2022-01-13 02:35:27 +0700

    add inventory animation

--- Prototype/Assets/Scripts/UI System/UImanager.cs
+++ Prototype/Assets/Scripts/UI System/UImanager.cs
@@ -6,10 +6,9 @@ using Chronos;
 public class UImanager : MonoBehaviour
 {
     [SerializeField] GameObject inventory;
-   /*SerializeField] GameObject pause;*/
     [SerializeField] GameObject map;
+    [SerializeField] Animator animator;
 
-    private Clock enemyClock, playerClock, bulletClock;
     private bool isPaused = false;
 
     private void Update()
@@ -19,6 +18,7 @@ public class UImanager : MonoBehaviour
             if (isPaused)
             {
                 CloseInventory();
+                animator.SetTrigger("CloseInventory");
             }
             else
             {
@@ -37,75 +37,41 @@ public class UImanager : MonoBehaviour
                 OpenMap();
             }
         }
-
-        /*
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
-            {
-                Resume();
-            } else
-            {
-                Pause();
-            }
-        }*/
     }
-
-    // resume & pause
-    /* private void Pause()
-     {
-         pause.SetActive(true);
-         Time.timeScale = 0;
-         /*enemyClock.localTimeScale = 0;
-         playerClock.localTimeScale = 0;
-         bulletClock.localTimeScale = 0;
-    isPaused = true;
-    }
-
-    public void Resume()
-    {
-        pause.SetActive(false);
-        Time.timeScale = 1;
-        /*enemyClock.localTimeScale = 1;
-        playerClock.localTimeScale = 1;
-        bulletClock.localTimeScale = 1;
-        isPaused = false;
-}
-*/
 
     // inventory
-private void OpenInventory()
+    private void OpenInventory()
     {
         inventory.SetActive(true);
-        Time.timeScale = 0;
-        /*enemyClock.localTimeScale = 0;
-        playerClock.localTimeScale = 0;
-        bulletClock.localTimeScale = 0;*/
-isPaused = true;
+        SetPauseTimescale();
     }
-
     private void CloseInventory()
     {
         inventory.SetActive(false);
-        Time.timeScale = 1;
-        /*enemyClock.localTimeScale = 1;
-        playerClock.localTimeScale = 1;
-        bulletClock.localTimeScale = 1;*/
-        isPaused = false;
+        SetResumeTimescale();
     }
 
     // map
     private void OpenMap()
     {
         map.SetActive(true);
-        Time.timeScale = 0;
-        isPaused = true;
+        SetPauseTimescale();
     }
-
     private void CloseMap()
     {
         map.SetActive(false);
-        Time.timeScale = 1;
+        SetResumeTimescale();
+    }
+
+    // set timescale
+    public void SetPauseTimescale()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void SetResumeTimescale()
+    {
+        Time.timeScale = 1f;
         isPaused = false;
     }
 }
