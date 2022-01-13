e59b98c2ceaedba898320b89ae06adf39067ce32
thellost <aldy10ball@gmail.com>
2022-01-14 02:24:12 +0700

    conflict solve

--- Prototype/Assets/ShootScript.cs
+++ Prototype/Assets/ShootScript.cs
@@ -8,12 +8,14 @@ public class ShootScript: MonoBehaviour
     [SerializeField] GameObject flare;
     [SerializeField] Transform muzzle;
     [SerializeField] Transform target;
+    [SerializeField] AudioClip shootSound;
     [SerializeField] float cameraShakeIntensity = 5;
     [SerializeField] float cameraShakeFrequency = 1;
     [SerializeField] float cameraShakeTimer = 0.1f;
     private float offset = 180f;
     private BulletManager bulletManager;
     private Transform bulletSpawnDump;
+
     private void Awake()
     {
         bulletManager = GameObject.FindGameObjectWithTag("Spawner").GetComponent<BulletManager>();
@@ -21,6 +23,7 @@ public class ShootScript: MonoBehaviour
     }
     private void fireBullet()
     {
+        SoundManager.Instance.PlaySFX(shootSound, 1, true);
 
         GameObject particle = bulletManager.GenerateVXFromPool(flare, bulletSpawnDump, muzzle.transform.position, Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z));
         particle.GetComponent<Transform>().Rotate(new Vector3(0, 0, offset));
