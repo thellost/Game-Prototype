2fe2ffd551711ef5e798ccdb27c92d6614bd3172
thellost <aldy10ball@gmail.com>
2022-01-13 17:12:43 +0700

    fix

--- Prototype/Assets/Scripts/Decorative/introDialog.cs
+++ Prototype/Assets/Scripts/Decorative/introDialog.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class introDialog : MonoBehaviour, IOnDialogExit
 {
+
+    [SerializeField] int nextIndexScene;
+    private void Awake()
+    {
+        if(nextIndexScene == null)
+        {
+            nextIndexScene = SceneManager.GetActiveScene().buildIndex + 1;
+        }
+    }
     public void DialogExit()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(nextIndexScene);
     }
 }
